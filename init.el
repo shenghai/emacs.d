b90d435bfad35fb0987ef457d78d1a69897f8fae
shenghai.geng <shenghai.geng@gmail.com>
2014-11-10 13:23:24 +0800

    fix china characters can not be displayed issue

--- init.el
+++ init.el
@@ -138,7 +138,7 @@
 ;;----------------------------------------------------------------------------
 ;; Locales (setting them earlier in this file doesn't work in X)
 ;;----------------------------------------------------------------------------
-(require 'init-locales)
+;;(require 'init-locales)
 
 (add-hook 'after-init-hook
           (lambda ()
